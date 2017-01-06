6cad653bbee3fbaf039e94608287ab9e99543197
ulrike <rika087@yahoo.de>
2017-01-06 17:23:37 +0100

    set up pymongo and first REST API

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify
-<<<<<<< HEAD
 from flask.ext.pymongo import PyMongo
 from pymongo import MongoClient
 from flask_mongoalchemy import MongoAlchemy
@@ -16,17 +15,6 @@ app.config["MONGO_DBNAME"] = "wmt-test"
 mongo = PyMongo(app, config_prefix='MONGO')
 
 
-=======
-from pymongo import MongoClient
-from flask_mongoalchemy import MongoAlchemy
-
-
-app = Flask(__name__)
-
-# MongoAlchemy konfiguriern
-app.config['MONGOALCHEMY_DATABASE'] = 'library'
-db = MongoAlchemy(app)
->>>>>>> 94fefce18d6fb5e3b14f302735060626cf397014
 
 # only a few routes here, Angular does the rest
 
@@ -48,7 +36,6 @@ def page_not_found(e):
 #def favicon():
 #    return
 
-<<<<<<< HEAD
 # Setup the Api here
 restApi = Api(app)
 restApi.add_resource(Users, "/api/users/")
@@ -70,36 +57,9 @@ if __name__ == "__main__":
 
 
 
-=======
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/api/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
->>>>>>> 94fefce18d6fb5e3b14f302735060626cf397014
 
 
 
 
-<<<<<<< HEAD
-=======
-	
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
->>>>>>> 94fefce18d6fb5e3b14f302735060626cf397014
 
 
