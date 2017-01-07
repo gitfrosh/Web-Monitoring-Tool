ca7c66c6a32ead10f28bc95d433c0065c3eb07e3
ulrike <rika087@yahoo.de>
2017-01-07 01:26:17 +0100

    Angular talking to REST

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,64 @@
 'use strict';
 
+
 var myApp = angular.module('myApp', [
- 'ngRoute',
-]);
+ 'ngRoute', 'ngResource'
+]).controller('Hello', function($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+});
+
+
+myApp.factory('User', function($resource) {
+  return $resource("/api/users/:id", {}, {
+    query: { method: "GET", isArray: false }
+  });
+});
+
+
+myApp.controller('UserLoginCtrl', function($scope, User) {
+  // Get specific User, curently only used for Hello-Statement in top bar
+   User.get({id: "586f791f73fa964fe89bac81"}, function(data) {
+   $scope.user = data;
+  });
+
+});
+
+
+myApp.controller('UserCtrl', function($scope, User) {
+  // Get all users
+   User.query(function(data) {
+    $scope.users = data.users;
+  });
+
+  // Our form data for creating a new User with ng-model
+  $scope.postData = {};
+  $scope.newUser = function() {
+    var user = new User($scope.postData);
+    user.$save();
+  };
+
+  // Get specific User with the following Id ... (hardcoded)
+  User.get({id: "586f791f73fa964fe89bac81"}, function(data) {
+    $scope.user = data;
+    //console.log($scope.user);
+
+    // now give only the user's topics
+    $scope.usertopics = data.USER.topics;
+
+    // put the topics' titles in an array
+    $scope.usertopictitles = [];
+
+    for (var i = 0, l = $scope.usertopics.length; i < l; i++) {
+        $scope.usertopictitles.push($scope.usertopics[i].title);
+        }
+    console.log($scope.usertopictitles);
+
+
+})
+});
 
 myApp.config(['$routeProvider',
      function($routeProvider) {
@@ -16,8 +72,7 @@ myApp.config(['$routeProvider',
                  // get uUser topics in array from DB
                  // show the first (if available), and fill in site elements
 
-                // http://stackoverflow.com/questions/26239071/how-to-show-mvc-logged-in-username-with-angularjs
-
+                 controller: 'UserCtrl',
                  templateUrl: '../static/partials/topic-view.html',
              }).
              when('/dashboard/newsletter', {
