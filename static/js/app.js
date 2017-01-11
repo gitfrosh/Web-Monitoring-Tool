db2dc47d64d9894039c23fd870a4c12fceb580a7
ulrike <rika087@yahoo.de>
2017-01-12 00:59:45 +0100

    more backend functionalities

--- static/js/app.js
+++ static/js/app.js
@@ -2,135 +2,161 @@
 
 
 var myApp = angular.module('myApp', [
- 'ngRoute', 'ngResource'
+    'ngRoute', 'ngResource'
 ]);
 
 
 myApp.factory('Document', function($resource) {
-  return $resource("/api/documents/:query", {}, {
-    query: { method: "GET", isArray: false }
-  });
-});
-
-
-myApp.controller('DocumentsCtrl', function($scope, Document) {
-
-    // fetch queryId from UserCtrl
-
-    // Get specific Documents
-   Document.get({query: queryId}, function(data) {
-   $scope.documents = data;
-   console.log($scope.documents);
-  });
-
+    return $resource("/api/documents/:query", {}, {
+        query: {
+            method: "GET",
+            isArray: false
+        }
+    });
 });
 
 
 myApp.factory('User', function($resource) {
-  return $resource("/api/users/:id", {}, {
-    query: { method: "GET", isArray: false }
-  });
+    return $resource("/api/users/:id", {}, {
+        query: {
+            method: "GET",
+            isArray: false
+        }
+    });
 });
 
 
 myApp.controller('UserLoginCtrl', function($scope, User) {
-  // Get specific User, curently only used for Hello-Statement in top bar
-   User.get({id: "5873de1dae29a94ff4c2edde"}, function(data) {
-   $scope.user = data;
-  });
+    // Get specific User, curently only used for Hello-Statement in top bar
+    User.get({
+        id: "5873de1dae29a94ff4c2edde"
+    }, function(data) {
+        $scope.user = data;
+    });
 
 });
 
 
-myApp.controller('UserCtrl', function($scope, User) {
-  // Get all users
-   User.query(function(data) {
-    $scope.users = data.users;
-  });
+myApp.controller('UserCtrl', function($scope, User,  Document) {
+    // this controller is actually too big and complex
 
-  // Our form data for creating a new User with ng-model
-  $scope.postData = {};
-  $scope.newUser = function() {
-    var user = new User($scope.postData);
-    user.$save();
-  };
+    // Get all users
+    User.query(function(data) {
+        $scope.users = data.users;
+    });
 
-  // Get specific User with the following Id ... (hardcoded)
-  User.get({id: "5873de1dae29a94ff4c2edde"}, function(data) {
-    $scope.user = data;
-    //console.log($scope.user);
+    // Our form data for creating a new User with ng-model
+    $scope.postData = {};
+    $scope.newUser = function() {
+        var user = new User($scope.postData);
+        user.$save();
+    };
 
-    // now give only the user's topics
-    $scope.usertopics = data.USER.topics;
+    // Get specific User with the following Id ... (hardcoded)
+    User.get({
+        id: "5873de1dae29a94ff4c2edde"
+    }, function(data) {
+        $scope.user = data;
+        //console.log($scope.user);
 
-
-    $scope.getContent = function(usertopics){
-     return usertopics.value + " " + obj.text;
-     };
-
-
-    // put the topics' titles in an array
-    $scope.usertopictitles = [];
+        // now give only the user's topics
+        $scope.usertopics = data.USER.topics;
 
 
-    for (var i = 0, l = $scope.usertopics.length; i < l; i++) {
-        $scope.usertopictitles.push($scope.usertopics[i].title);
-       // console.log($scope.querys.push(data.USER.topics.querys[i]))
+        $scope.getContent = function(usertopics) {
+            return usertopics.value + " " + obj.text;
+        };
+
+
+        // put the topics' titles in an array
+        $scope.usertopictitles = [];
+
+
+        for (var i = 0, l = $scope.usertopics.length; i < l; i++) {
+            $scope.usertopictitles.push($scope.usertopics[i].title);
+
         }
 
+        // default value of Dropdown: empty space
+        $scope.myDropDown = "";
 
-    $scope.myDropDown = $scope.usertopictitles[0];
-    console.log("myDropDown=" +$scope.myDropDown);
+
+        // actions to do on selected item
+        $scope.selectAction = function() {
 
 
-    $scope.selectAction = function() {
-    console.log("myDropDown=" + $scope.myDropDown);
-    //console.log($scope.usertopics);
-    $scope.querysForSelectedTopic =   _.find($scope.usertopics, function(item) {
-                return item.title ===  $scope.myDropDown;
-                    });
-    console.log($scope.querysForSelectedTopic);
+            // here give only the user's topics' querys - so that matching documents can be retrieved
+            $scope.querysForSelectedTopic = _.find($scope.usertopics, function(item) {
+                return item.title === $scope.myDropDown;
+            });
 
-    // here give only the user's topics' querys - so that matching documents can be retrieved
-    console.log($scope.querysForSelectedTopic.querys);
+            // bind IDs of querys ...
+            $scope.iDsOfquerysForSelectedTopic = $scope.querysForSelectedTopic.querys;
+
+
+            // for every saved query-ID find connected documents
+             for (var i = 0, l = $scope.iDsOfquerysForSelectedTopic.length; i < l; i++) {
+
+                 // start a fresh documentCollection for our output table
+                 $scope.documentCollection = [];
+
+                 // get the documents
+                 Document.get({
+                      query: $scope.iDsOfquerysForSelectedTopic[i].$oid
+
+                 }, function(data) {
+                   $scope.documents = data;
+
+                   // add outputs of more than one query to the collection
+                   $scope.documentCollection.push($scope.documents.DOCUMENTS);
+
+                   // flatten the array
+                   $scope.documentCollection = _.flatten($scope.documentCollection);
+
+                      console.log($scope.documentCollection);
+                            }
+
+             )}
+
 
         };
 
 
-      
-})
+
+    })
 });
 
 myApp.config(['$routeProvider',
-     function($routeProvider) {
-         $routeProvider.
-             when('/', {
-                 redirectTo: '/dashboard/'
-             }).
-             when('/dashboard', {
-                 // Show elements depending on user details:
-                 // Does user have any topics saved?
-                 // get uUser topics in array from DB
-                 // show the first (if available), and fill in site elements
+    function($routeProvider) {
+        $routeProvider.
+        when('/', {
+            redirectTo: '/dashboard/'
+        }).
+        when('/dashboard', {
+            // Show elements depending on user details:
+            // Does user have any topics saved?
+            // get uUser topics in array from DB
+            // show the first (if available), and fill in site elements
 
-                 controller: 'UserCtrl',
-                 templateUrl: '../static/partials/topic-view.html',
-             }).
-             when('/dashboard/newsletter', {
-                 templateUrl: '../static/partials/newsletter.html',
-             }).
-             when('/dashboard/userconfig', {
-                 templateUrl: '../static/partials/user-config.html',
-             }).
-             when('/dashboard/usersources', {
-                 templateUrl: '../static/partials/user-sources.html',
-             }).
-             when('/dashboard/logout', {
-                 controller: 'Logout'
-             }).
-             otherwise({
-                 redirectTo: '/dashboard/'
-             })
-         
-         ;
-    }]);
+            controller: 'UserCtrl',
+            templateUrl: '../static/partials/topic-view.html',
+        }).
+        when('/dashboard/newsletter', {
+            templateUrl: '../static/partials/newsletter.html',
+        }).
+        when('/dashboard/userconfig', {
+            templateUrl: '../static/partials/user-config.html',
+        }).
+        when('/dashboard/usersources', {
+            templateUrl: '../static/partials/user-sources.html',
+        }).
+        when('/dashboard/logout', {
+            controller: 'Logout'
+        }).
+        otherwise({
+            redirectTo: '/dashboard/'
+        })
+
+        ;
+    }
+]);
